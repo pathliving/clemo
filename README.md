9e9109aa2ec5b0b3f7ef3b92dc216b908cf01951
Andrii Hrynenko <pathliving@gmail.com>
2019-04-22 11:27:00 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,23 @@
+How to start
+===
+You need [git](https://git-scm.com/) and [node.js](https://nodejs.org/) on your computer before running
+
+1) clone repository
+
+		git clone https://github.com/pathliving/clemo.git clemo
+
+2) go to 'clemo' directory
+
+		cd clemo
+
+3) install dependencies
+
+		npm install
+
+4) start project
+
+		npm start
+
+5) to create a dist folder
+
+		npm run build
---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('offline-plugin/runtime').install();
+
+// fonts
+import './fonts/montserrat-hairline-webfont.woff';
+import './fonts/montserrat-hairline-webfont.woff2';
+import './fonts/montserrat-hairline-webfont.ttf';
+
+import './fonts/montserrat-light-webfont.woff';
+import './fonts/montserrat-light-webfont.woff2';
+import './fonts/montserrat-light-webfont.ttf';
+
+import './fonts/montserrat-regular-webfont.woff';
+import './fonts/montserrat-regular-webfont.woff2';
+import './fonts/montserrat-regular-webfont.ttf';
+
+import './fonts/montserrat-bold-webfont.woff';
+import './fonts/montserrat-bold-webfont.woff2';
+import './fonts/montserrat-bold-webfont.ttf';
+
+// etc
+import './index.html';
+import './index.scss';
+import './scripts/script.js';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
